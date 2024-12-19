b291ad11a6e39a6f29fb6bef59137fa90214e228
Rosario Rendina <161160724+RosarioRendina@users.noreply.github.com>
2024-12-19 12:04:09 +0100

    java maven start

---
+++ noleggio/src/test/java/com/generation/org/noleggio/NoleggioVeicoliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.org.noleggio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoleggioVeicoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
