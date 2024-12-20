f80ac51303063da04e74afe4860f981e51189069
Giovanni Accarino <183734715+GiovanniAccarino@users.noreply.github.com>
2024-12-20 19:15:45 +0100

    cambiamenti generali e inizio js

--- noleggio/src/main/resources/templates/js/camelCar.js
+++ noleggio/src/main/resources/templates/js/camelCar.js
@@ -1,1 +1,22 @@
 var swiper = new Swiper(".mySwiper", {});
+
+const key = 'http://localhost:8080/api/veicolo';
+
+const veicoli = [];
+
+
+fetch(key)
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Errore nella richiesta');
+    }
+    return response.json();
+})
+.then(data => {
+    console.log(data); 
+    veicoli.push(data);
+    
+})
+.catch(error => {
+    console.error('Errore:', error);
+});
